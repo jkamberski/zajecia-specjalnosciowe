70706d93870d2d778d18059420ad775c35eba7b0
Jakub Kamberski <kubakamberski@gmail.com>
2026-02-01 16:28:13 +0100

    Delete microblog/src/main/java/pl/microblog/FollowerDao.java

--- microblog/src/main/java/pl/microblog/FollowerDao.java
+++
@@ -1,7 +0,0 @@
-package pl.atins.microblog.dao;
-
-public interface FollowerDao {
-    void follow(long followerId, long followedId);
-    void unfollow(long followerId, long followedId);
-    boolean isFollowing(long followerId, long followedId);
-}
