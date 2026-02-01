fa70ce185f38548eb980ce85550254d14de64eb9
Jakub Kamberski <kubakamberski@gmail.com>
2026-02-01 21:27:07 +0100

    Add username field to Uzytkownik model

--- microblog/src/main/java/pl/atins/microblog/model/Uzytkownik.java
+++ microblog/src/main/java/pl/atins/microblog/model/Uzytkownik.java
@@ -10,6 +10,18 @@ public class Uzytkownik {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
